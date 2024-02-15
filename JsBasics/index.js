6d463d012ce7df54aeb7ed32182a028f65df8b69
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-02-15 16:20:09 +0530

    Update index.js

--- JsBasics/index.js
+++ JsBasics/index.js
@@ -1,1 +1,16 @@
+// dynamic typed language 
 console.log("Namaste Dunia")
+// Named memory location is variable by let
+
+let a = true;
+console.log(a);
+
+// let vs var difference is of scope
+// var is global 
+// let is local
+
+let names = "name";
+console.log(names);
+
+names = 2;
+console.log(names);
