d2920646a1a293c7819411ef54625e384cc7d029
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-02-19 15:54:53 +0530

    new files added

--- JsBasics/indexJSDOM2.js
+++ JsBasics/indexJSDOM2.js
@@ -1,4 +1,20 @@
-function clcik(){
-    console.log('i have clicked');
+// function clcik(){
+//     console.log('i have clicked');
+// }
+// document.addEventListener('click' ,clcik);
+
+// const content = document.querySelector('#wrapper');
+// content.addEventListener('click',function(event){console.log(event)});
+
+let myDiv = document.createElement('div');
+
+for(let i=0;i<100;i++){
+    let newElement = document.createElement('p');
+    newElement.textContent = 'This is para ' + i;
+
+    newElement.addEventListener('click',function(event){console.log(`I have clicked the para ${i}`)});
+
+    myDiv.appendChild(newElement);
 }
-document.addEventListener('click' ,clcik);
+
+document.body.appendChild(myDiv);
