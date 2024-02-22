17bc9a617027731abdb9c54476d00ea910d63a3e
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-02-22 21:41:03 +0530

    assets added

--- jsProjects/weatherapp/script.js
+++ jsProjects/weatherapp/script.js
@@ -1,31 +1,42 @@
-const api = "f27c81e2ae10b3f5626690bb00d2d69c";
+// const api = "f27c81e2ae10b3f5626690bb00d2d69c";
 
-async function showWeather(){
+// async function showWeather(){
 
 
-    try{
-        let city = "goa";
+//     try{
+//         let city = "goa";
         
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`);
-        const data = await response.json();
+//         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`);
+//         const data = await response.json();
 
-        console.log("Weather data -> " + data);
+//         console.log("Weather data -> " + data);
     
-        let newPara = document.createElement('p');
-        newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
+//         let newPara = document.createElement('p');
+//         newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
     
-        document.body.appendChild(newPara);
-        // renderWeatherInfo(data);
-    }
+//         document.body.appendChild(newPara);
+//         // renderWeatherInfo(data);
+//     }
 
-    catch(err){
-        console.log("error");
-    }
+//     catch(err){
+//         console.log("error");
+//     }
 
-};
+// };
 
 
-function switchTab(clickedTab){
-    apiErrorContainer.classList.remove("active");
-}
+// function switchTab(clickedTab){
+//     apiErrorContainer.classList.remove("active");
+// }
 
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-conatiner");
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
+let currentTab = userTab;
+const API_KEY = "f27c81e2ae10b3f5626690bb00d2d69c";
+currentTab.classList.add("current-tab");
