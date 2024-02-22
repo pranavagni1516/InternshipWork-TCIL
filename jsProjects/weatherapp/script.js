b6c25bba475eddb302f08c80966640b6e30a9f95
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-02-22 18:21:24 +0530

    Update script.js

--- jsProjects/weatherapp/script.js
+++ jsProjects/weatherapp/script.js
@@ -1,29 +1,31 @@
-console.log("first");
+const api = "f27c81e2ae10b3f5626690bb00d2d69c";
 
 async function showWeather(){
 
 
     try{
-
-        let lat = 15.3333;
-        let lon = 74.0833;
         let city = "goa";
-        let api = "f27c81e2ae10b3f5626690bb00d2d69c";
+        
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`);
-    
-    
-    
         const data = await response.json();
+
         console.log("Weather data -> " + data);
     
         let newPara = document.createElement('p');
         newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
     
         document.body.appendChild(newPara);
+        // renderWeatherInfo(data);
     }
 
     catch(err){
-
+        console.log("error");
     }
 
 };
+
+
+function switchTab(clickedTab){
+    apiErrorContainer.classList.remove("active");
+}
+
