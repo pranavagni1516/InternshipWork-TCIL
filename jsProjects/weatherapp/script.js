6936ee421ee2f44349f56f8853140671a32ec9eb
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-02-23 13:47:37 +0530

    adding js functionality

--- jsProjects/weatherapp/script.js
+++ jsProjects/weatherapp/script.js
@@ -46,8 +46,66 @@ function switchTab(clickedTab){
         currentTab.classList.remove("current-tab");
         currentTab = clickedTab;
         currentTab.classList.add("current-tab");
+
+        if(!searchForm.classList.contains("active")){
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active");
+        }
+
+        else {
+            userInfoContainer.classList.remove("active");
+            searchForm.classList.remove("active");
+            getfromSessionStorage();
+        }
     }
 };
+
+function getfromSessionStorage(){
+    const localCoordinates = sessionStorage.getItem("user-coordinates");
+
+    if(!localCoordinates){
+        grantAccessContainer.classList.add("active");
+    }
+    else{
+        const coordinates = JSON.parse(localCoordinates);
+        fetchWeatherInfo(coordinates);
+    }
+};
+
+async function fetchWeatherInfo(coordinates){
+    const {lat,lon} = coordinates;
+    // make grant invisible
+    grantAccessContainer.classList.remove("active");
+    // make loader visible
+    loadingScreen.classList.add("active");
+
+    // API call
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+        const data = await response.JSON();
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    }
+    catch(err){
+        loadingScreen.classList.remove("active");
+        console.log("Error");
+    }
+};
+
+function renderWeatherInfo(data){
+    const cityName = document.querySelector("[data-cityName]");
+    const countryIcon = document.querySelector("[data-countryIcon]");
+    const desc = document.querySelector("[data-weatherDesc]");
+    const weatherIcon = document.querySelector("[data-weatherIcon]]");
+    const temp = document.querySelector("[data-Temp]]");
+    const windspeed = document.querySelector("[data-windspeed]]");
+    const humidity = document.querySelector("[data-humidity]]");
+    const cloudiness = document.querySelector("[data-cloudiness]]");
+};
+
+
 
 userTab.addEventListener("click",()=>{
     // pass clicked tab as input
