14460d9c9a2ee94d309fbab6c2f6e2541d36c670
pranavagni1516 <pranav.agni1516@gmail.com>
2024-02-25 13:54:53 +0530

    completed

--- reactProjects/NikeBrand Page/src/components/Hero.jsx
+++ reactProjects/NikeBrand Page/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Herosection = () => {
   return (
-    <main className='hero'>
+    <main className='hero container'>
         <div className='hero-content'>
         <h1>YOUR FEET DESERVE THE BEST</h1>
         <p>
@@ -10,7 +10,7 @@ const Herosection = () => {
         </p>
         <div className='hero-btn'>
             <button>Shop Now</button>
-            <button>Category</button>
+            <button className='secondary-btn'>Category</button>
         </div>
         <div className='shopping'>
             <p>Also Available On</p>
