87a58fc3d724dd29f0d5bd9a5debbbdc07215a7e
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-02-26 17:41:00 +0530

    adding jsx components

---
+++ reactProjects/docsApp/src/components/Background.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Background() {
+  return (
+    <>
+    <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold'>Documents</div>
+    <h1 className='absolute top-1/2 text-[13vw] -translate-x-[50%] -translate-y-[50%] left-1/2 leading-none tracking-tighter '>Docs.</h1>
+
+    </>
+  )
+}
+
+export default Background;
