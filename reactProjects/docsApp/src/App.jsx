1a094cefdd2aec885654ba53f9048b889bdcf033
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-02-26 19:22:15 +0530

    Creating Card Component and using the jsx

--- reactProjects/docsApp/src/App.jsx
+++ reactProjects/docsApp/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Background from './components/Background.jsx';
+import Background from './components/Background';
+import Forground from './components/Forground'
 
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800 font-roboto'>
       <Background/>
+      <Forground/>
     </div>
   )
 }
