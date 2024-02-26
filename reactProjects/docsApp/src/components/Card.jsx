2f5d14b0d2e4e1b0fae65c07ae8bc1df7a6c8b91
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-02-26 21:04:56 +0530

    card component complete

--- reactProjects/docsApp/src/components/Card.jsx
+++ reactProjects/docsApp/src/components/Card.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import {FaRegFileAlt} from "react-icons/fa";
+import {LuDownload} from "react-icons/lu";
 function Card() {
   return (
     <>
-    <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90  '></div>
+    <div className='relative w-60 h-72 rounded-[45px]  bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
+        <FaRegFileAlt/>
+        <p className='text-sm leading-tight mt-5 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        <div className='footer absolute bottom-0 w-full    left-0'>
+          <div className='flex items-center justify-between py-3 px-8 mb-5'>
+            <h5>.4mb</h5>
+            <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
+            <LuDownload size=".7em" color='#fff'/>
+            </span>
+          </div>
+          <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
+            <h3 className='tect-sm font-semibold'>Download Now</h3>
+          </div>
+        </div>
+    </div>
 
     </>
   )
--- reactProjects/docsApp/src/components/Forground.jsx
+++ reactProjects/docsApp/src/components/Forground.jsx
@@ -6,6 +6,7 @@ function Forground() {
     <>
       <div className='fixed top-0 left-0 z-[3] w-full h-full'>
         <Card/>
+        <Card/>
       </div>
     </>
   )
