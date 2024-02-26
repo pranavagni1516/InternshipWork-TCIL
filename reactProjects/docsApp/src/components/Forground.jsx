c34098827db49be12933c417b943d3b68f56b3fd
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-02-26 21:41:50 +0530

    data mapped dynamically to cards

--- reactProjects/docsApp/src/components/Forground.jsx
+++ reactProjects/docsApp/src/components/Forground.jsx
@@ -2,11 +2,32 @@ import React from 'react'
 import Card from './Card.jsx'
 
 function Forground() {
+    const data = [
+        {
+            desc:"Lorem ipsum, dolor sit amet consectetur adipisicing.",
+            fileSize:".9 mb",
+            close: false,
+            tag:{isOpen:true,tagTitle:"Download Now",tagColor:"green"},
+        },
+        {
+            desc:"Lorem ipsum, dolor sit amet consectetur adipisicing.",
+            fileSize:".9 mb",
+            close: false,
+            tag:{isOpen:true,tagTitle:"Download Now",tagColor:"green"},
+        },
+        {
+            desc:"Lorem ipsum, dolor sit amet consectetur adipisicing.",
+            fileSize:".9 mb",
+            close: false,
+            tag:{isOpen:true,tagTitle:"Download Now",tagColor:"green"},
+        },
+    ];
   return (
     <>
-      <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-        <Card/>
-        <Card/>
+      <div className='fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap'>
+        {data.map((item,index)=>(
+            <Card data={item}/>
+        ))}
       </div>
     </>
   )
