44f7da9856e40af96039d5d939f9f26f6a94a647
pranavagni1516 <pranav.agni1516@gmail.com>
2024-02-27 18:30:10 +0530

    React Navbar Router and creation

---
+++ reactProjects/NavRoute/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className='relative w-full h-screen bg-zinc-800 font-roboto'>
+      
+    </div>
+  )
+}
+
+export default App
