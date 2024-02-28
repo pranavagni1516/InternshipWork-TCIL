0de041c373339aec7e5f2df763cfaf252980d67c
pranavagni1516 <pranav.agni1516@gmail.com>
2024-02-28 13:26:30 +0530

    component folder structure created

---
+++ reactProjects/testimonialCard/src/components/Testimonials.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './Card'
+const Testimonials = () => {
+  return (
+    <div>
+    {
+        <Card/>
+    }
+    </div>
+  )
+}
+
+export default Testimonials
