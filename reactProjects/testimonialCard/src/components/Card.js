24bb668792f3d8d316b4d41131b6f4f7e10d2d30
pranavagni1516 <pranav.agni1516@gmail.com>
2024-02-28 23:39:28 +0530

    tailwind css added

--- reactProjects/testimonialCard/src/components/Card.js
+++ reactProjects/testimonialCard/src/components/Card.js
@@ -1,19 +1,30 @@
 import React from 'react'
+import {FaQuoteLeft,FaQuoteRight,FaGem} from 'react-icons/fa';
 
 const Card = (props) => {
     let review = props.review;
   return (
-    <div>
-        <div>
-            <img src={review.image} alt="" />
-
+    <div className='flex flex-col md:relative '>
+        <div className='absolute top-[-7rem] z-[10] mx-auto'>
+            <img className='aspect-square rounded-full w-[140px] h-[140px] z-25' src={review.image} alt="" />
+            <div className='w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-10] left-[10px]'></div>
         </div>
-        <div>
-            <p>{review.name}</p>
+        <div className='text-center mt-7'>
+            <p className='font-bold text-2xl capitalize'>{review.name}</p>
         </div>
-        <div>
-            <p>{review.job}</p>
+        <div className='text-center mt-7'>
+            <p className='text-sm text-violet-300 uppercase'>{review.job}</p>
         </div>
+        <div className='mx-auto text-violet-400 mt-5'>
+            <FaQuoteLeft/>
+        </div>
+        <div className='text-center mt-4 text-slate-500'>
+            {review.text}
+        </div>
+        <div className='mx-auto text-violet-400 mt-5'>
+            <FaQuoteRight/>
+        </div>
+       
     </div>
   )
 }
--- reactProjects/testimonialCard/src/components/Testimonials.js
+++ reactProjects/testimonialCard/src/components/Testimonials.js
@@ -1,14 +1,38 @@
 import React from 'react'
 import Card from './Card'
+import {FiChevronLeft,FiChevronRight} from 'react-icons/fi';
 
 
 const Testimonials = (props) => {
+
     let reviews = props.reviews;
-  return (
-    <div>
-    {
-        <Card review={reviews[0]}/>
+
+    function leftShiftHandler(){
+        
     }
+    function rightShiftHandler(){
+
+    }
+    function surpriseMeHandler(){
+
+    }
+
+  return (
+    <div className='flex flex-col mx-auto space-y-6 w-[700px] bg-slate-100 mt-4 rounded-md'>
+    
+        <Card review={reviews[0]}/>
+        <div className='text-3xl flex mt-5 gap-3 text-violet-400 font-bold mx-auto'>
+            <button onClick={leftShiftHandler} className='cursor-pointer hover:text-violet-500'>
+                <FiChevronLeft/>
+            </button>
+            <button onClick={rightShiftHandler} className='cursor-pointer hover:text-violet-500'>
+                <FiChevronRight/>
+            </button>
+        </div>
+        <div>
+            <button onClick={surpriseMeHandler} className='bg-violet-400 mb-4 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded:md font-bold rounded-md text-white text-lg'>Surprise me</button>
+        </div>
+    
     </div>
   )
 }
