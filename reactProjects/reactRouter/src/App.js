80f6ec501b20b1e3997d74e9f3e1faae47580110
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-03-01 00:58:36 +0530

    Adding react Routes

--- reactProjects/reactRouter/src/App.js
+++ reactProjects/reactRouter/src/App.js
@@ -1,7 +1,23 @@
+import { Routes ,Route} from "react-router";
 import "./App.css";
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+      <div>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+        </Routes>
+      </div>
+    );
 }
 
 export default App;
