9a6c242888eb2388cccf1312b20f9ee1892bb120
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-03-01 03:41:53 +0530

    tailwind css added

--- reactProjects/reactRouter/src/components/Navbar.jsx
+++ reactProjects/reactRouter/src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import {React} from 'react'
 import {Link} from 'react-router-dom'
-import logo from "../assets/Logo.svg"
+// import logo from "../assets/Logo.svg"
 import toast from "react-hot-toast"
 
 const Navbar = (props) => {
     let isLoggedIn = props.isLoggedIn;
     let setIsLoggedIn = props.setIsLoggedIn;
   return (
-    <div className='flex justify-evenly'>
+    <div className='flex justify-between items-center w-11/12 max-w-[1160px] py-4 mx-auto'>
         <Link to="/">
-            <img src={logo} alt='err' width={160} height={32} loading='lazy'/>
+            {/* <img src={logo} alt='err' width={160} height={32} loading='lazy'/> */}
+            <h2 className='text-white drop-shadow-sm  text-2xl font-extrabold'>TCIL-IT</h2>
         </Link>
 
         <nav>
-            <ul className='flex gap-3'>
+            <ul className='flex gap-x-6 text-white'>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
@@ -27,18 +28,18 @@ const Navbar = (props) => {
         </nav>
 
         {/* LOGIN SIGNUP LOGOUT DASHBOARD */}
-        <div className='flex ml-5 mr-3 gap-3'>
+        <div className='flex items-center gap-x-4'>
             {   !isLoggedIn &&
-                <Link to="/login"><button>Login</button></Link>
+                <Link to="/login"><button className=' bg-richblack-800 text-white py-[8px] px-[12px] rounded-[8px] border border-richblack-700'>Login</button></Link>
             }
             {   !isLoggedIn &&
-                <Link to="/signup"><button>Sign Up</button></Link>
+                <Link to="/signup"><button className=' bg-richblack-800 text-white py-[8px] px-[12px] rounded-[8px] border border-richblack-700'>Sign Up</button></Link>
             }
             {   isLoggedIn &&
-                <Link to="/"><button onClick={()=>{setIsLoggedIn(false);toast.success("Logged Out");}}>Log Out</button></Link>
+                <Link to="/"><button className=' bg-richblack-800 text-white py-[8px] px-[12px] rounded-[8px] border border-richblack-700' onClick={()=>{setIsLoggedIn(false);toast.success("Logged Out");}}>Log Out</button></Link>
             }
             {   isLoggedIn &&
-                <Link to="/dashboard"><button>Dashboard</button></Link>
+                <Link to="/dashboard"><button className=' bg-richblack-800 text-white py-[8px] px-[12px] rounded-[8px] border border-richblack-700'>Dashboard</button></Link>
             }
         </div>
     </div>
