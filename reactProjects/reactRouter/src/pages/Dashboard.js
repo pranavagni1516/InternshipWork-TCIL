f81ea7968c88cc6f4eee77dce9dde87d894be95c
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-03-01 03:55:33 +0530

    minor changes

--- reactProjects/reactRouter/src/pages/Dashboard.js
+++ reactProjects/reactRouter/src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='flex justify-center items-center'>Dashboard</div>
+    <div className='flex justify-center items-center h-full text-white text-3xl'>Welcome To TCIL-IT</div>
   )
 }
 
