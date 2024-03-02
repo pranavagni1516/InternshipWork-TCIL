1b1cf5705d17747576a388df8ad4741245d2f569
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-03-02 13:48:04 +0530

    complete with .env issue fixed

--- reactProjects/gifGen/src/components/Random.jsx
+++ reactProjects/gifGen/src/components/Random.jsx
@@ -1,27 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 import Spin  from './Spin.jsx';
+import useGif from '../hooks/useGif.js';
 
 
 
 const Random = () => {
-  const [gif,setGif] = useState('');
-  const [loading,setLoading] = useState(false);
-  
-  
-  async function fetchData(){
-    setLoading(true);
-    // const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=kPP90iBwNSiKi0RADfd9CT56vKdKzeFp&tag=&rating=g`;
-    const {data} = await axios.get(url);
-    const imageSource = data.data.images.downsized_large.url;
-    setGif(imageSource);
-    setLoading(false);
-  }
-  useEffect(()=>{
-    fetchData();
-  },[])
 
+  const {gif,loading,fetchData} = useGif();
   function clickHandler(){
     fetchData();
   }
--- reactProjects/gifGen/src/hooks/useGif.js
+++ reactProjects/gifGen/src/hooks/useGif.js
@@ -1,5 +1,28 @@
-const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-const useGif = (tag) => {};
+
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+
+const useGif = (tag) => {
+    const [gif,setGif] = useState('');
+    const [loading,setLoading] = useState(false);
+   
+    
+    async function fetchData(){
+      setLoading(true);
+      const {data} = await axios.get(!tag ? `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g` :`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}&rating=g`);
+      const imageSource = data.data.images.downsized_large.url;
+      setGif(imageSource);
+      setLoading(false);
+    }
+    useEffect(()=>{
+      fetchData();
+    },[])
+  
+    return {gif,loading,fetchData};
+};
 
 export default useGif;
