7f4fb14db283d287a6385967c535381f1bcc3310
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-03-02 13:19:43 +0530

    changeHandler and useSate

--- reactProjects/gifGen/src/App.jsx
+++ reactProjects/gifGen/src/App.jsx
@@ -4,7 +4,7 @@ import Tag from "./components/Tag"
 
 export default function App() {
   return (
-    <div className="w-full h-screen flex flex-col background items-center">
+    <div className="w-full flex flex-col justify-evenly space-y-4 background items-center gap-y-2">
       <div className=" w-[1200px] h-15 rounded-lg mx-auto mt-5 flex justify-center text-center items-center p-5 bg-white text-4xl font-bold border border-black ">RANDOM GIFS</div>
       <div className="flex flex-col ">
         <Random/>
--- reactProjects/gifGen/src/components/Tag.jsx
+++ reactProjects/gifGen/src/components/Tag.jsx
@@ -1,3 +1,44 @@
-export default function Tag() {
-  return <div>Tag</div>;
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Spin  from './Spin.jsx';
+
+
+
+const Tag = () => {
+  const [gif,setGif] = useState('');
+  const [loading,setLoading] = useState(false);
+  const [tag,setTag] = useState('car');
+  function changeHandler(event){
+    setTag(event.target.value)
+  }
+  
+  async function fetchData(){
+    setLoading(true);
+    // const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=kPP90iBwNSiKi0RADfd9CT56vKdKzeFp&tag=&rating=g`;
+    const {data} = await axios.get(url);
+    const imageSource = data.data.images.downsized_large.url;
+    setGif(imageSource);
+    setLoading(false);
+  }
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  function clickHandler(){
+    fetchData();
+  }
+
+  return (
+    <div className='w-[900px]  mt-5 flex flex-col rounded-[40px] bg-blue-500 items-center mx-auto justify-evenly border border-black gap-y-4 mb-[20px]'>
+      <h2 className='text-4xl font-bold border underline border-none mt-4'>RANDOM GIF</h2>
+      <div className='overflow-hidden w-[450px] h-[300px] mt-5 flex items-center justify-center mx-auto'>
+        {loading ? (<Spin/>):(<img src={gif} alt="" width="450"/>)}
+      </div>
+      <input className='w-[450px] text-lg text-center py-2 rounded-lg mb-[20px]' value={tag} onChange={changeHandler} type="text" />
+      <button className='w-[500px] h-10  rounded-[25px] text-center  bg-yellow-500 hover:bg-blue-300 transition-all duration-200 mb-10' onClick={clickHandler}>GENERATE</button>
+    </div>
+  )
 }
+
+export default Tag
