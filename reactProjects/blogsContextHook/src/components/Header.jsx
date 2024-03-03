407f8f1b5249fc3f8587dc1c0803d54d4515d635
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-03-04 01:05:41 +0530

    Update Header.jsx

--- reactProjects/blogsContextHook/src/components/Header.jsx
+++ reactProjects/blogsContextHook/src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-        <header className='text-4xl font-extrabold flex mx-auto items-center justify-center p-5'>
+    <div className='border shadow-md py-2'>
+        <header className='text-4xl font-extrabold flex mx-auto items-center justify-center p-3'>
             TCIL - IT BLOGS
         </header>
     </div>
