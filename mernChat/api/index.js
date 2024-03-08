93b91dc634f7e0611b036c10509bffd9b157ccc5
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-03-08 14:48:05 +0530

    get/post not woring

---
+++ mernChat/api/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose =require('mongoose');
+const dotenv = require('dotenv');
+const User = require('./models/User');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+jwtSecret = process.env.JWT_SECRET;
+const app = express();
+
+app.use(cors({
+    credentials:true,
+    origin:process.env.CLIENT_URL,
+}));
+
+app.get('/test',(req,res)=>{
+    res.json('test ok');
+});
+
+app.post('/register',async (req,res)=>{
+    const {username,password} = req.body;
+    const createdUser = await User.create({username,password})
+    jwt.sign({userId:createdUser._id},jwtSecret,{},(err,token)=>{
+        if(err) throw err;
+        res.cookie('token',token).status(201).json('ok');
+    });
+});
+
+app.listen(4040);
+// gVCs3Bi46oGrkjsx
--- mernChat/mernChat/src/App.jsx
+++ mernChat/mernChat/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import Register from "./Register"
+import axios from "axios"
 
 function App() {
- 
+ axios.defaults.baseURL = 'http://localhost:4040';
+ axios.defaults.withCredentials = true;
 
   return (
     <>
