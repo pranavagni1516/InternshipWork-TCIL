b68a9a6b98a7cfdd0af6ae0e18b26383b20fe173
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-13 20:25:41 +0530

    user auth mapping

--- mernChat/backend/controllers/auth.controller.js
+++ mernChat/backend/controllers/auth.controller.js
@@ -1,5 +1,13 @@
-export const signup = (req,res) =>{
-    res.send("signup karloooo");
-    console.log("signup user");}
-export const login = (req,res) =>{console.log("login user");}
-export const logout = (req,res) =>{console.log("logout user");}
+export const signup = async (req,res) =>{
+    try {
+        const {fullName,username,confirmPassword,gender} = req.body;
+        if(password !== confirmPassword){
+            return res.status(400).json({error:"Passwords dont match"})
+        } 
+        const user = await User.findOne({username});
+    } catch (error) {
+        
+    }
+};
+export const login = (req,res) =>{console.log("login user");};
+export const logout = (req,res) =>{console.log("logout user");;}
--- mernChat/backend/server.js
+++ mernChat/backend/server.js
@@ -1,17 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDb from "./db/connectToMongoDB.js";
 
 const app = express();
 dotenv.config();
+app.use(express.json());
+app.use("/api/auth",authRoutes);
+
 const PORT = process.env.PORT || 5000
 
 app.get("/",(req,res)=>{
     res.send("server is ready on port");
 });
 
-app.use("/api/auth",authRoutes);
 
-app.listen(PORT,()=>
+app.listen(PORT,()=>{
+    connectToMongoDb();
     console.log(`server running at port ${PORT}`)
+}
 );
