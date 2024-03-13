a4f042d14983f1a07de175d0b2a6975038c59fb2
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-14 01:18:31 +0530

    Update auth.controller.js

--- mernChat/backend/controllers/auth.controller.js
+++ mernChat/backend/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user.model.js"
+
 export const signup = async (req,res) =>{
     try {
         const {fullName,username,confirmPassword,gender} = req.body;
@@ -5,6 +7,24 @@ export const signup = async (req,res) =>{
             return res.status(400).json({error:"Passwords dont match"})
         } 
         const user = await User.findOne({username});
+        if(user){
+            return res.status(400).json({error:"User Already exist"})
+        }
+        //HASH password here
+        //Profile pic here
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+            fullName,username,confirmPassword,gender,profilePic:gender === 'male' ? boyProfilePic : girlProfilePic
+        })
+        await newUser.save();
+        res.status(201).json({
+            _id: newUser._id,
+            fullName:newUser.fullName,
+            username:newUser.username,
+            profilePic:newUser.profilePic
+        });
     } catch (error) {
         
     }
