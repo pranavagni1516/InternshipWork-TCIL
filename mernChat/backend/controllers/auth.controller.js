56fa59ef898634386ca09b7ec018f55e9febe4ca
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-13 19:45:12 +0530

    major change in the project and full ob updation

---
+++ mernChat/backend/controllers/auth.controller.js
@@ -0,0 +1,5 @@
+export const signup = (req,res) =>{
+    res.send("signup karloooo");
+    console.log("signup user");}
+export const login = (req,res) =>{console.log("login user");}
+export const logout = (req,res) =>{console.log("logout user");}
