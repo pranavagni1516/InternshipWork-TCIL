5ceb7843102d38cfa1af1054fa5ea9c0defe0e1a
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-15 00:14:32 +0530

    token

--- mernChat/backend/utils/generateToken.js
+++ mernChat/backend/utils/generateToken.js
@@ -1,6 +1,10 @@
 import jwt from 'jsonwebtoken';
 const generateTokenAndSetCookie = (userID,res)=>{
-    const token = jwt.sign({userID},process.env.JWT_SECRET,{expiresIN:"15d",});
-    res.cookie("jwt",token,{maxAge:15*24*60*60*1000,httpOnly:true,sameSite:"strict"});
+    const token = jwt.sign({userID},process.env.JWT_SECRET,{expiresIn:'15d'});
+    res.cookie("jwt",token,{
+        maxAge:15*24*60*60*1000,
+        httpOnly:true,
+        sameSite:"strict"
+    });
 }
 export default generateTokenAndSetCookie;
