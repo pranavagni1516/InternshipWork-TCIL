67b7e00cb4a67acffe7134a0d7632f8d2bf5537b
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-14 19:42:51 +0530

    auth token

--- mernChat/backend/controllers/auth.controller.js
+++ mernChat/backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
-import bcryptjs form "bcryptjs";
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req,res) =>{
     try {
@@ -12,6 +13,8 @@ export const signup = async (req,res) =>{
             return res.status(400).json({error:"User Already exist"})
         }
         //HASH password here
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password,salt);
         //Profile pic here
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -19,18 +22,23 @@ export const signup = async (req,res) =>{
         const newUser = new User({
             fullName,
             username,
-            password,
+            password:hashPassword,
             confirmPassword,
             gender,
             profilePic:gender === "male" ? boyProfilePic : girlProfilePic
         })
-        await newUser.save();
-        res.status(201).json({
-            _id: newUser._id,
-            fullName:newUser.fullName,
-            username:newUser.username,
-            profilePic:newUser.profilePic
-        });
+        if(newUser){
+            generateTokenAndSetCookie(newUser._id,res); 
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName:newUser.fullName,
+                username:newUser.username,
+                profilePic:newUser.profilePic
+            });
+        }else{
+            res.status(400).json({error:"invalid user data"});
+        }
     } catch (error) {
         console.log("Error in signup controller",error.message);
         res.status(500).json({error:"Internal server error"});
