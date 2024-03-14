c1963374bb89bf6eb16a693768b1dd77d8458782
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-14 23:31:18 +0530

    Update auth.controller.js

--- mernChat/backend/controllers/auth.controller.js
+++ mernChat/backend/controllers/auth.controller.js
@@ -29,6 +29,7 @@ export const signup = async (req,res) =>{
         })
         if(newUser){
             generateTokenAndSetCookie(newUser._id,res); 
+            
             await newUser.save();
             res.status(201).json({
                 _id: newUser._id,
