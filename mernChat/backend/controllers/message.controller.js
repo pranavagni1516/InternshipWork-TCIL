9ec05d719fad4a43a0739792ed362e7a154273da
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-15 13:10:57 +0530

    protecting route middle ware

--- mernChat/backend/controllers/message.controller.js
+++ mernChat/backend/controllers/message.controller.js
@@ -1,3 +1,34 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
 export const sendMessage = async (req,res)=>{
-    console.log("message sent",req.params.id);
+    try {
+        const {message} = req.body;
+        const {id:receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let conversation = await Conversation.findOne({
+            participants:{$all:[senderId,receiverId]},
+        })
+
+        if(!conversation){
+            conversation = await Conversation.create({
+                participants:[senderId,receiverId],
+            })
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message
+        })
+
+        if(newMessage){
+            conversation.messages.push(newMessage._id);
+        }
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.log("Error in sendMessage controller",error.message);
+        res.status(500).json({error:"Interval server error in msgcontroller"});
+    }
 };
--- mernChat/backend/server.js
+++ mernChat/backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDb from "./db/connectToMongoDB.js";
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
