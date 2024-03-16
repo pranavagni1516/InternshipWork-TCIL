b4bfd832fcdd31aa0401cbf9ef578bfe9b9e5e55
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-17 01:12:54 +0530

    controller login structure

---
+++ backendExpress/fileUpload/controllers/fileUpload.js
@@ -0,0 +1,18 @@
+const File = require("../models/File");
+
+//localFileUpload
+exports.localFileUpload = async (req,res)=>{
+    try {
+        const file = req.files.file;
+        console.log("File fetched",file);
+
+        let path = __dirname + "/files/"+Date.now();
+        console.log("PATH ->",path);
+
+        file.mv(path,(err)=>{console.log(err);});
+
+        res.json({success:true,messgae:"Local file uploaded successfully"})
+    } catch (error) {
+        console.log(error);
+    }
+}
