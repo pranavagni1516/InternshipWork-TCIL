26c8c52378a45a8ab526c8dc2cddc17a54e5c5cc
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-17 00:49:37 +0530

    index file complete and server running

--- backendExpress/fileUpload/index.js
+++ backendExpress/fileUpload/index.js
@@ -1,7 +1,23 @@
 // app
+const express = require("express");
+const app = express();
 // port 
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 // middleware
+app.use(express.json());
+const fileupload = require("express-fileupload");
+app.use(fileupload());
 // db
+const db = require("./config/database");
+db.connect();
 // connect cloud
+const cloudinary = require("./config/cloudinary");
+cloudinary.cloudinaryConnect();
 // api route
+const Upload = require("./routes/FileUpload");
+app.use('api/v1/upload',Upload);
 // activate server
+app.listen(PORT,()=>{
+    console.log(`App running at ${PORT}`);
+})
