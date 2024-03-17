83003d9361e33cb452c30b194f4ab12cad1c17aa
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-17 12:15:38 +0530

    getMessages controller working

--- mernChat/backend/controllers/message.controller.js
+++ mernChat/backend/controllers/message.controller.js
@@ -43,14 +43,14 @@ export const sendMessage = async (req,res)=>{
 export const getMessages = async (req,res)=>{
     try {
         const {id:userToChatId} = req.params;
-        const senderId = req.user._id;
+        const senderID = req.user._id;
         const conversation = await Conversation.findOne({
             participants:{$all:[senderID,userToChatId]},
         }).populate("messages");
 
         res.status(200).json(conversation.messages);
     } catch (error) {
-        console.log("Error in sendMessage controller",error.message);
-        res.status(500).json({error:"Interval server error in msgcontroller"});
+        console.log("Error in getMessage controller",error.message);
+        res.status(500).json({error:"Interval server error in get msgcontroller"});
     }
 };
