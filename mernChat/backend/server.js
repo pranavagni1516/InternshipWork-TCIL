f6bdcb34cbc8bbb75e16cb39aa7e8ffddf92d7ad
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-17 12:39:08 +0530

    Get Users for sidebar endpoint

--- mernChat/backend/server.js
+++ mernChat/backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDb from "./db/connectToMongoDB.js";
 
@@ -11,8 +13,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
 
 
 // app.get("/",(req,res)=>{
