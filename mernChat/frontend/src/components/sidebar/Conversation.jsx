578ad28204084fa62207df9e8cbb4ca0810ab5ad
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-18 14:02:55 +0530

    side bar created with conversations and logout button

--- mernChat/frontend/src/components/sidebar/Conversation.jsx
+++ mernChat/frontend/src/components/sidebar/Conversation.jsx
@@ -5,15 +5,23 @@ const Conversation = () => {
     <>
 
         <div className='flex gap-2 rounded hover:bg-blue-500 p-2 py-1 cursor-pointer'>
-                    <div className='avatar online'> 					<div className='w-12 rounded-full'>
+                    <div className='avatar online'>
+                     		<div className='w-12 rounded-full'>
                             <img
                                     src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png'
                                 alt='user avatar'
                             />
+                            </div>
+                    </div>
+                    <div className='flex flex-col flex-1'>
+                        <div className='flex gap-3 justify-between'>
+                            <p className='text-xl font-bold text-gray-200'>John Doe</p>
+                            <span className='text-xl '>🎃</span>
                         </div>
                     </div>
         </div>
 
+       
 
         <div className='divider my-0 py-0 h-1'></div>
     </>
--- mernChat/frontend/src/components/sidebar/Sidebar.jsx
+++ mernChat/frontend/src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Searchinput from "./Searchinput"
-
+import Conversations from './Conversations'
+import LogoutButton from '../sidebar/LogoutButton'
 const Sidebar = () => {
   return (
-    <div>
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
         <Searchinput/>
         <div className='divider px-3'></div>
+        <Conversations/>
+        <div className='w-full h-5 bg-transparent'></div>
+        <LogoutButton/>
     </div>
   )
 }
