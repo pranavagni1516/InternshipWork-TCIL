a9841cbb5bbe52322752c7d0a45b212b8911582d
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-20 19:09:00 +0530

    minor updates

--- mernChat/frontend/src/App.jsx
+++ mernChat/frontend/src/App.jsx
@@ -4,9 +4,10 @@ import Home from "./pages/home/Home"
 function App(){
   return (
     <div className="p-4 h-screen resize flex items-center justify-center">
-      {/* <Login/> */}
-      {/* <SignUp/> */}
       <Home/>
+      {/* <Login/> */}
+      <Home/>
+      {/* <SignUp/> */}
     </div>
   )
 }
