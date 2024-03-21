7347ff2bc222abe6db9ffad9f7c96bdd03ab8f9d
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-22 01:08:05 +0530

    created Auth Functionality

--- mernChat/frontend/src/App.jsx
+++ mernChat/frontend/src/App.jsx
@@ -3,20 +3,19 @@ import SignUp from "./pages/signup/SignUp";
 import Home from "./pages/home/Home"
 import { Navigate,Routes,Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-// import { useAuthContext } from "./context/AuthContext";
+import { useAuthContext } from "./context/AuthContext";
 
 function App(){
-  // const {authUser} = useAuthContext();
+  const {authUser} = useAuthContext();
   return (
     <div className="p-4 h-screen resize flex items-center justify-center">
       {/* <Home/> */}
       {/* <Login/> */}
       {/* <SignUp/> */}
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        {/* <Route path="/signup" element={authUser ? <Navigate to="/"/>:<SignUp/>} /> */}
+        <Route path="/" element={!authUser?<Navigate to={"/login"}/>:<Home/>} />
+        <Route path="/login" element={authUser ? <Navigate to="/"/> :<Login/>} />
+        <Route path="/signup" element={authUser ? <Navigate to="/"/>:<SignUp/>} />
       </Routes>
       <Toaster/>
     </div>
--- mernChat/frontend/src/main.jsx
+++ mernChat/frontend/src/main.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-// import  {AuthContext} from './context/AuthContext.jsx'
+import  {AuthContextProvider} from './context/AuthContext'
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <AuthContext.Provider> */}
+      <AuthContextProvider>
         <App />
-      {/* </AuthContext.Provider> */}
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
