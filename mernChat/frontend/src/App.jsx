efca993bb8ee0c4830e9aec9cdb13ae4a298c897
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-03-21 23:32:31 +0530

    sign-up functionality patrially completed

--- mernChat/frontend/src/App.jsx
+++ mernChat/frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import Home from "./pages/home/Home"
+import { Routes,Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 function App(){
   return (
     <div className="p-4 h-screen resize flex items-center justify-center">
-      <Home/>
+      {/* <Home/> */}
       {/* <Login/> */}
       {/* <SignUp/> */}
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
+      </Routes>
+      <Toaster/>
     </div>
   )
 }
