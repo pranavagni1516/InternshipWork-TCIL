35df161203cc7d5b177d8d0d844f975825c1d003
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-22 02:10:34 +0530

    minor loading spinner css added

--- mernChat/frontend/src/pages/signup/SignUp.jsx
+++ mernChat/frontend/src/pages/signup/SignUp.jsx
@@ -77,7 +77,9 @@ const SignUp = () => {
                   Already have an account?
                 </Link>
                 <div>
-                  <button className='btn btn-block btn-sm mt-2'>Sign-Up</button>
+                   <button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+							        {loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
+						       </button>
                 </div>
               </form>
             </div>
