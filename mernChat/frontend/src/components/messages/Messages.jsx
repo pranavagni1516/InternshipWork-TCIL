5c17bf2b27de55a881acf1b4dbf136b081c9344e
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-25 18:30:04 +0530

    changes in messages

--- mernChat/frontend/src/components/messages/Messages.jsx
+++ mernChat/frontend/src/components/messages/Messages.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeleton/MessageSkeleton";
 import Message from './Message'
+
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
   return (
     <div className='px-4 flex-1 overflow-auto'>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
+        {/* {!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} ref={lastMessageRef}>
+						<Message message={message} />
+					</div>
+				))} */}
+
+			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{!loading && messages.length === 0 && (
+				<p className='text-center'>Send a message to start the conversation</p>
+			)}
     </div>
   )
 }
