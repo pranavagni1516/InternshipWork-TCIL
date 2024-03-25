e74f8138b95889f7816eed20c3801104462d874f
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-25 12:15:14 +0530

    starting send message functionality

--- mernChat/frontend/src/components/messages/MessageContainer.jsx
+++ mernChat/frontend/src/components/messages/MessageContainer.jsx
@@ -2,6 +2,7 @@ import {React,useEffect} from 'react'
 import Messages from './Messages'
 import MessageInput from './MessageInput'
 import {TiMessages} from 'react-icons/ti'
+import { useAuthContext } from "../../context/AuthContext";
 import useConversation from '../../zustand/useConversation'
 
 const MessageContainer = () => {
@@ -21,7 +22,7 @@ const MessageContainer = () => {
             <>
             {/* Header */}
             <div className='bg-transparent px-4 py-2 mb-2'>
-                <span className='label-text'>To:</span> <span className='text-grey-900 font-bold'>{selectedConversation.fullName}</span>
+                <span className='label-text'>To: </span> <span className='text-grey-900 font-bold'>{ selectedConversation.fullName }</span>
             </div>
             <Messages/>
             <MessageInput/>
@@ -35,10 +36,11 @@ export default MessageContainer
 
 const NoChatSelected = ()=>{
     // const {selectedConversation,setSelectedConversation} = useConversation();
+    const { authUser } = useAuthContext();
     return(
         <div className='flex items-center justify-center w-full h-full'>
 			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-				<p>Welcome 👋 ❄</p>
+				<p>Welcome 👋  { authUser.fullName }  ❄</p>
 				<p>Select a chat to start messaging</p>
 				<TiMessages className='text-3xl md:text-6xl text-center' />
 			</div>
