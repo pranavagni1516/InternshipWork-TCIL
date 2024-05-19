10e7ea580c7bc58235aabf00d464da5f9f746eee
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2024-05-19 13:21:29 +0530

    updated as per the requirements

--- mainLanding/src/components/About.jsx
+++ mainLanding/src/components/About.jsx
@@ -9,15 +9,16 @@ function About() {
       <div className="w-full flex gap-5 border-t-[1px] pt-10 mt-20 border-[#a1b562]">
         <div className="w-1/2">
           <h1 className="text-7xl">Our Approach</h1>
-          <button className="flex uppercase  gap-10 items-center px-10 py-6 bg-zinc-900  mt-10 rounded-full text-white">
-            Read More
+          <button className="flex uppercase  gap-10 items-center px-10 py-6 bg-zinc-900  mt-10 rounded-full text-white" >
+            <a href="https://tcilitchandigarh.com/about.html">Read More</a>
             <div className="w-3 h-3 bg-zinc-100 rounded-full hover:scale-150 transform duration-200"></div>
           </button>
         </div>
         <div className="w-1/2 h-[70vh] rounded-3xl ">
           <img
             className="w-full h-full object-cover rounded-xl"
-            src="https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-663x469.jpg"
+            // src="https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-663x469.jpg"
+            src="https://tcilitchandigarh.com/gallery/thumbnails/6weeks_summer_training_chandigarh_mohali.jpg"
             alt="photo"
           />
         </div>
